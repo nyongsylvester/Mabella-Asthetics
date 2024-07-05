0873caa9e0be5877e4b27f68b2c40811f74a6e0a
Sylvester Nyong <92986433+nyongsylvester@users.noreply.github.com>
2024-07-05 02:31:48 +0000

    Add files via upload

---
+++ Carmel make-up/script.js
@@ -0,0 +1,14 @@
+// ===============================HOME=======================
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    // _autoplay: {
+    //     delay: 500,
+    //     disableOnInteraction: false,
+    // },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
